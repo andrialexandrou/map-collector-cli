e40d0f6a057d04d6ffe2fab250ad8f5c32115677
Andri Alexandrou <andri.j.alexandrou@gmail.com>
2019-11-10 19:41:42 +0200

    feat: filter results so only relevant new data are stores

--- datapull/retrieve-from-bls.js
+++ datapull/retrieve-from-bls.js
@@ -1,8 +1,35 @@
 const makeRequest = require('../utils/make-request');
 const flatten = require('lodash.flatten');
 const getBlsCodesPromise = require('../utils/get-bls-codes');
+const remove = require('lodash.remove')
+
+function isRelevant(pullname) {
+  if (pullname === 'employment') {
+    return obj => parseInt(obj.year, 10) >= 1999
+  }
+  if (pullname === 'recovery') {
+    const first = 'Dec-07'
+    return function (obj) {
+      return parseInt(obj.year, 10) === 2007 ?
+        obj.period === 'M12' :
+        parseInt(obj.year, 10) >= 2008;
+    }
+  }
+}
+isRelevant.test = function() {
+  console.assert(isRelevant('employment')({year: '1998', period: 'M01'}) === false, 'M01 1998 should be irrelevant')
+  console.assert(isRelevant('employment')({year: '1998', period: 'M12'}) === false, 'M12 1998 should be irrelevant')
+  console.assert(isRelevant('employment')({year: '1999', period: 'M01'}) === true, 'M01 1999 should be relevant')
+  console.assert(isRelevant('employment')({year: '1999', period: 'M12'}) === true, 'M12 1999 should be relevant')
+
+  console.assert(isRelevant('recovery')({year: '2007', period: 'M11'}) === false, 'M11 2007 should be irrelevant')
+  console.assert(isRelevant('recovery')({year: '2007', period: 'M12'}) === true, 'M12 2007 should be relevant')
+  console.assert(isRelevant('recovery')({year: '2008', period: 'M05'}) === true, 'M05 2008 should be relevant')
+  console.assert(isRelevant('recovery')({year: '2017', period: 'M06'}) === true, 'M06 2017 should be relevant')
+}
 
 module.exports = {
+  isRelevant,
   recovery: opts => {
     let recoveryCodes;
     return getBlsCodesPromise
@@ -17,6 +44,7 @@ module.exports = {
         return Promise.all(recoveryCodes.map(arr => makeRequest(arr, opts)));
       })
       .then(flatten)
+      .then(arr => arr.filter(isRelevant('recovery')))
       .catch(err => {
         throw new Error(err);
       });
@@ -35,8 +63,20 @@ module.exports = {
         return Promise.all(employmentCodes.map(arr => makeRequest(arr, opts)));
       })
       .then(flatten)
+      .then(arr => arr.filter(isRelevant('employment')))
       .catch(err => {
         throw new Error(err);
       });
   }
 };
+
+
+if (process.env.NODE_ENV === 'test') {
+  const functions = Object.keys(module.exports);
+  functions.forEach(fn => {
+    const thisFn = module.exports[fn];
+    if (thisFn.test) {
+      thisFn.test();
+    }
+  });
+}
