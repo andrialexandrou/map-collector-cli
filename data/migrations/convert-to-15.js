80f07d2708872e727ffb5d7a532439f20985baf4
Andri Alexandrou <andri.j.alexandrou@gmail.com>
2019-11-10 19:41:55 +0200

    fix: correct some linting/testing errors

--- data/migrations/convert-to-15.js
+++ data/migrations/convert-to-15.js
@@ -1,3 +1,10 @@
+const db = require('../../datapull/db-config.js');
+const obj = {};
+
+if (process.env.NODE_ENV === 'test') {
+  return;
+}
+
 db.serialize(() => {
   // db.serialize(() => {
   db.all(
